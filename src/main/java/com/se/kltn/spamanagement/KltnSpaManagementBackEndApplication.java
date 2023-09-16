4d41e737a38786ad146fe378ec5272d4d238d7f9
vietho <hcv2501@gmail.com>
2023-09-16 17:05:41 +0700

    init project

---
+++ src/main/java/com/se/kltn/spamanagement/KltnSpaManagementBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.se.kltn.spamanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KltnSpaManagementBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KltnSpaManagementBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/se/kltn/spamanagement/KltnSpaManagementBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.kltn.spamanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KltnSpaManagementBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
